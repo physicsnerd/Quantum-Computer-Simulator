63f84bb09bb42b87abd42c8cfd7845a83c5cb776
physicsnerd <heather.nerd@gmail.com>
2016-12-03 19:57:48 -0600

    Update main-file.py

--- main-file.py
+++ main-file.py
@@ -3,7 +3,7 @@ from random import randint
 import cmath
 import math
 
-singates = ["Hadamard":hadop, "X":xop, "Z":zop, "Y":yop, "sqrtX":sqrtxop,"phase shift":phaseshiftop,"measurement":measurement,"custom":customop]
+singates = ["Hadamard":hadop, "X":xop, "Z":zop, "Y":yop, "sqrtX":sqrtxop,"phase shift":phaseshiftop,"measurement":measurement,"custom":customop, "control":control, "target":target]
 twgates = ["cNOT", "swap"]
 thrgates = ["Toffoli"]
 qubits = float(input("How many qubits would you like to use? (Currently, only supports 1): "))
@@ -89,6 +89,27 @@ def measurement(qstat):
     elif prob1 < random:
         qstat = np.array([1,0])
 
+def control(qstat):
+    typegat = input("Which gate is this the control qubit for? See list of two qubit gates at the top.")
+    if typegat == "cNOT":
+        mem1 = qstat
+    elif typegat == "swap":
+        mem2 = qstat
+
+def target(qstat):
+    typegat2 = input("Which gate is this target qubit for? See list of two qubit gates at the top.")
+    if typegat2 == "cNOT":
+        if np.array_equal(mem3, [0,1]) == True:
+            qstat = qstat
+        elif np.array_equal(mem3, [1,0]) == True:
+            qstat = np.dot(qstat,mem3)
+        else:
+            print("superposition...not implemented")
+    elif typegat2 == "swap":
+        qstat = mem4
+    else:
+        print("other gates not yet implemented")
+
 while done == "n":
     if qubits == 1:
         fstgat = input("what g ate would you like to use? use the list of single gates at the top: ")
@@ -101,127 +122,13 @@ while done == "n":
     elif qubits == 2:
         while done2 == "n":
             fstgat = input("what gate would you like to use for 1st qubit? Use the list of single qubits at the top, plus control or target")
-            if fstgat == "Hadamard":
-                qstat = hadop(qstat)
-                done2 = input("Done with your 1st qubit? y or n: ")
-            elif fstgat == "X":
-                qstat = xop(qstat)
-                done2 = input("Done with your 1st qubit? y or n: ")
-            elif fstgat == "Z":
-                qstat = zop(qstat)
-                done2 = input("Done with your 1st qubit? y or n: ")
-            elif fstgat == "Y":
-                qstat = yop(qstat)
-                done2 = input("Done with your 1st qubit? y or n: ")
-            elif fstgat == "sqrtX":
-                qstat = sqrtxop(qstat)
-                done2 = input("Done with your 1st qubit? y or n: ")
-            elif fstgat == "phase shift":
-                phasepos = [math.pi/4, math.pi/2]
-                print(phasepos)
-                x = input("Please pick one of the two phase shifts, 0 for the first, 1 for the second: ")
-                if x == "0":
-                    y = phasepos[0]
-                elif x == "1":
-                    y = phasepos[1]
-                qstat = phaseshiftop(qstat,y)
-                done2 = input("Done with your 1st qubit? y or n: ")
-            elif fstgat == "measurement":
-                prob1 = probability(qstat,0)
-                prob2 = probability(qstat,1)
-                random = randint(0,1)
-                if random <= prob1:
-                    qstat = np.array([0,1])
-                elif prob1 < random:
-                    qstat = np.array([1,0])
-                    done2 = input("Done with your 1st qubit? y or n: ")
-            elif fstgat == "custom":
-                num1 = float(input("Please input a number (no pi, e, etc) for the first number in your matrix (row 1 column 1): "))
-                num2 = float(input("Number for matrix - row 1 column 2: "))
-                num3 = float(input("Number for matrix - row 2 column 1: "))
-                num4 = float(input("Number for matrix - row 2 column 2: "))
-                qstat = customop(qstat,num1,num2,num3,num4)
-                done2 = input("Done with your 1st qubit? y or n: ")
-            elif fstgat == "control":
-                typegat = input("Which gate is this the control qubit for? See list of two qubit gates at the top.")
-                if typegat == "cNOT":
-                    mem1 = qstat
-                elif typegat == "swap":
-                    mem2 = qstat
-                done2 = input("Done with your 1st qubit? y or n")
-            elif fstgat == "target":
-                typegat2 = input("Which gate is this target qubit for? See list of two qubit gates at the top.")
-                if typegat2 == "cNOT":
-                    if np.array_equal(mem3, [0,1]) == True:
-            	        qstat = qstat
-                    elif np.array_equal(mem3, [1,0]) == True:
-            	        qstat = np.dot(qstat,mem3)
-                    else:
-            	        print("superposition...not implemented")
-                elif typegat2 == "swap":
-                    qstat = mem4
-                done2 = input("Done with your 1st qubit? y or n")
+            if fstgat in singates:
+                qstat = singates[fstgat](qstat)
+		done2 = input("Done with your 1st qubit? y or n: ")     
         scndgat = input("what gate would you like to use for 2nd qubit? Use the list of single qubits at the top, plus control or target")
-        if scndgat == "Hadamard":
-            qstat = hadop(qstat)
+        if scndgat in singates:
+            qstat = singates[scndgat](qstat)
             done = input("Done with your circuit? y or n: ")
-        elif scndgat == "X":
-            qstat = xop(qstat)
-            done = input("Done with your circuit? y or n: ")
-        elif scndgat == "Z":
-            qstat = zop(qstat)
-            done = input("Done with your circuit? y or n: ")
-        elif scndgat == "Y":
-            qstat = yop(qstat)
-            done = input("Done with your circuit? y or n: ")
-        elif scndgat == "sqrtX":
-            qstat = sqrtxop(qstat)
-            done = input("Done with your circuit? y or n: ")
-        elif scndgat == "phase shift":
-            phasepos = [math.pi/4, math.pi/2]
-            print(phasepos)
-            x = input("Please pick one of the two phase shifts, 0 for the first, 1 for the second: ")
-            if x == "0":
-                y = phasepos[0]
-            elif x == "1":
-                y = phasepos[1]
-            qstat = phaseshiftop(qstat,y)
-            done = input("Done with your circuit? y or n: ")
-        elif scndgat == "measurement":
-            prob1 = probability(qstat,0)
-            prob2 = probability(qstat,1)
-            random = randint(0,1)
-            if random <= prob1:
-                qstat = np.array([0,1])
-            elif prob1 < random:
-                qstat = np.array([1,0])
-            done = input("Done with your circuit? y or n: ")
-        elif scndgat == "custom":
-            num1 = float(input("Please input a number (no pi, e, etc) for the first number in your matrix (row 1 column 1): "))
-            num2 = float(input("Number for matrix - row 1 column 2: "))
-            num3 = float(input("Number for matrix - row 2 column 1: "))
-            num4 = float(input("Number for matrix - row 2 column 2: "))
-            qstat = customop(qstat,num1,num2,num3,num4)
-            done = input("Done with your circuit? y or n: ")
-        elif scndgat == "control":
-            typegat3 = input("Which gate is this the control qubit for? See list of two qubit gates at the top.")
-            if typegat3 == "cNOT":
-                mem3 = qstat
-            elif typegat3 == "swap":
-                mem4 = qstat
-            done = input("Done with your circuit? y or n")
-        elif scndgat == "target":
-            typegat4 = input("Which gate is this target qubit for? See list of two qubit gates at the top.")
-            if typegat4 == "cNOT":
-                if np.array_equal(mem1,[0,1]) == True:
-            	    qstat = qstat
-                elif np.array_equal(mem1, [1,0]) == True:
-            	    qstat = np.dot(qstat,mem3)
-                else:
-            	    print("superposition...not implemented")
-            elif typegat4 == "swap":
-                qstat = mem2
-            done = input("Done with your circuit? y or n")
     else:
         print("sorry, that functionality is not yet implemented")
         done = "y"
