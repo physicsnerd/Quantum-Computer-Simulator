d19cbb4807d88314b972b56a11273781726a92fc
physicsnerd <heather.nerd@gmail.com>
2016-12-03 20:00:07 -0600

    Update main-file.py

--- main-file.py
+++ main-file.py
@@ -4,6 +4,7 @@ import cmath
 import math
 
 singates = ["Hadamard":hadop, "X":xop, "Z":zop, "Y":yop, "sqrtX":sqrtxop,"phase shift":phaseshiftop,"measurement":measurement,"custom":customop, "control":control, "target":target]
+lstsingates = ["Hadamard", "X", "Z", "Y", "sqrtX", "phase shift", "measurement", "custom"]
 twgates = ["cNOT", "swap"]
 thrgates = ["Toffoli"]
 qubits = float(input("How many qubits would you like to use? (Currently, only supports 1): "))
@@ -21,7 +22,7 @@ else:
     qstat = np.array([0,1])
     qstat2 = np.array([0,1])
 
-print(singates)
+print(lstsingates)
 print(twgates)
 print(thrgates)
 
