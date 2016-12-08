47976725288045b2921115f809b5ab6843fce27f
physicsnerd <heather.nerd@gmail.com>
2016-12-08 16:33:23 -0600

    Update main-file.py

--- main-file.py
+++ main-file.py
@@ -46,7 +46,7 @@ while done == "n":
             x=1
             for i in commands[x]:
                 if i in singates:
-                    if i != "target" || (i == "target" & mem1 in globals()):
+                    if i != "target" | (i == "target" & mem1 in globals()):
                         qstat = singates[scndgat](qstat)
                     else:
                         x+=1
