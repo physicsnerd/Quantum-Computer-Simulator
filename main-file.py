a698855b4dca23db8f2f2cd96e1dee870f337345
physicsnerd <heather.nerd@gmail.com>
2016-12-11 16:23:26 -0600

    Update main-file.py

--- main-file.py
+++ main-file.py
@@ -36,29 +36,29 @@ while done == "n":
         commands = {}
         for i in range(1,qubits+1):
             commands[i] = []
-        x=1
-        while x <= qubits:
+        qubitnum=1
+        while qubitnum <= qubits:
             while done == "n":
-                fstgat = input("what gate would you like to use for " + str(x) + " qubit? Use the list of single qubits at the top, plus control or target: ")
-                commands[x].append(fstgat)
-                done = input("Done with your " + str(x) + " qubit? y or n: ")
-            x+=1
+                fstgat = input("what gate would you like to use for " + str(qubitnum) + " qubit? Use the list of single qubits at the top, plus control or target: ")
+                commands[qubitnum].append(fstgat)
+                done = input("Done with your " + str(qubitnum) + " qubit? y or n: ")
+            qubitnum+=1
             done = "n"
-        x=1
-        while x <= qubits:
-            for index,gate in enumerate(commands[x]):
+        qubitnum=1
+        while qubitnum <= qubits:
+            for index,gate in enumerate(commands[qubitnum]):
                 if gate in singates:
                     if gate != "target" or (gate == "target" and mem1 in globals()):
                         qstat = singates[gate](qstat)
                         print("done with a calculation")
-                        if index == len(commands[x]):
+                        if index == len(commands[qubitnum]):
                             print(" ")
                             print("your result is", qstat, "qubit #", index+1)
                             print("probability of |0> state is", probability(qstat,0))
                             print("probability of |1> state is", probability(qstat,1))
-                            x+=1
+                            qubitnum+=1
                     else:
-                        x+=1
+                        qubitnum+=1
                 else:
                     print(gate, " has not yet been implemented. Maybe try the custom gate?")
                     break
