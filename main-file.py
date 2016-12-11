f80158a3c8fd0f9d51f9d493f37ea29de5c2e4b9
physicsnerd <heather.nerd@gmail.com>
2016-12-11 17:37:54 -0600

    Update main-file.py

--- main-file.py
+++ main-file.py
@@ -8,12 +8,12 @@ qubits = int(input("How many qubits would you like to use? (Currently, only supp
 done = "n"
 qstatask = input("Would you like your initial qubits to be in the |0> state or |1> state? 0 or 1: ")
 if qstatask == "0":
-    qstat = {key:np.array([0,1]) for key in range(1,qubits+1)}
+    qstats = {key:np.array([0,1]) for key in range(1,qubits+1)}
 elif qstatask == "1":
-    qstat = {key:np.array([1,0]) for key in range(1,qubits+1)}
+    qstats = {key:np.array([1,0]) for key in range(1,qubits+1)}
 else:
     print("I'm sorry, that is not a valid input. State set to zero.")
-    qstat = {1:np.array([0,1])}
+    qstats = {key:np.array([0,1]) for key in range(1,qubits+1)}
 
 singates = {"Hadamard":hadop, "X":xop, "Z":zop, "Y":yop, "sqrtX":sqrtxop,"phase shift":phaseshiftop,"measurement":measurement,"custom":customop, "control":control, "target":target}
 twgates = ["cNOT", "swap"]
@@ -27,7 +27,9 @@ while done == "n":
     if qubits == 1:
         fstgat = input("what gate would you like to use? use the list of single gates at the top: ")
         if fstgat in singates:
+            qstat = qstats[1]
             qstat = singates[fstgat](qstat)
+            qstats[1] = qstat
             done = input("Done with your circuit? y or n: ")
         else:
             print("sorry, that gate is not yet implemented. maybe try custom gate.")
@@ -49,13 +51,15 @@ while done == "n":
             for index,gate in enumerate(commands[qubitnum]):
                 if gate in singates:
                     if gate != "target" or (gate == "target" and mem1 in globals()):
-                        qstat[index+1] = singates[gate](qstat[index+1])
+                        qstat = qstats[index+1]
+                        qstat = singates[gate](qstat)
+                        qstats[index+1] = qstat
                         print("done with a calculation")
                         if index+1 == len(commands[qubitnum]):
                             print(" ")
-                            print("your result is", qstat, "qubit #", index+1)
-                            print("probability of |0> state is", probability(qstat,0))
-                            print("probability of |1> state is", probability(qstat,1))
+                            print("your result is", qstats[index+1], "qubit #", index+1)
+                            print("probability of |0> state is", probability(qstats[index+1],0))
+                            print("probability of |1> state is", probability([qstats[index+1],1))
                     else:
                         print("checking for information")
                 else:
